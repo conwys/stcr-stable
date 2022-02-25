d92792ca02c02d3ed3e4bb69ab8d00072f3cf9e6
conwys <99252141+conwys@users.noreply.github.com>
2022-02-25 13:38:32 +0000

    Add files via upload

---
+++ static/editor.js
@@ -0,0 +1,79 @@
+function formatDoc(cmd, value=null) {
+    if(value) {
+        document.execCommand(cmd, false, value);
+    } else {
+        document.execCommand(cmd);
+    }
+}
+
+function link() {
+    const url = prompt('Type URL');
+    formatDoc('createLink', url);
+}
+
+const editor = document.getElementById('editor');
+
+editor.addEventListener('mouseenter', function() {
+    const linkClickable = editor.querySelectorAll('a');
+    linkClickable.forEach(item=> {
+        item.addEventListener('mouseenter', function() {
+            editor.setAttribute('contenteditable', true);
+            item.target='_blank';
+        })
+        item.addEventListener('mouseleave', function() {
+            editor.setAttribute('contenteditable', true);
+        })
+    })
+})
+
+const showCode = document.getElementById('show-code');
+let active = false;
+
+showCode.addEventListener('click', function () {
+	showCode.dataset.active = !active;
+	active = !active
+	if(active) {
+		editor.textContent = editor.innerHTML;
+		editor.setAttribute('contenteditable', false);
+	} else {
+		editor.innerHTML = editor.textContent;
+		editor.setAttribute('contenteditable', true);
+	}
+})
+
+function selectSize(value) {
+    if(value==='cus') {
+        const size = prompt('Type Size');
+        document.execCommand('fontSize', size);
+    } else {
+        document.execCommand('fontSize', value);
+    }
+}
+
+const filename = document.getElementById('filename');
+
+function fileHandle(value) {
+    if(value === 'new') {
+        editor.innerHTML = '';
+        filename.value = 'untitled';
+    } else if(value==='wrd') {
+        const htmContents = new Blob([editor.innerText]);
+        const url = URL.createObjectURL(htmContents);
+        const link = document.createElement('temp');
+        link.href = url;
+        link.download = `${filename.value}.docx`;
+        link.click();
+    } else if(value === 'pdf') {
+        html2pdf(editor).save(filename.value);
+    }
+}
+
+function save(filename, txt) {
+    var el = document.createElement('a');
+    el.setAttribute('href', 'editor.textContent' + encodeURIComponent(txt));
+    el.setAttribute('download', filename);
+    el.style.display = 'none';
+    document.body.appendChild(el);
+    el.click();
+    document.body.removeChild(el);
+}
